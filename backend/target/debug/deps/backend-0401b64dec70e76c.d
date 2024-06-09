cbe24acec8c9b23c89237ca863a9df9e4887b490
Johs Salinas <brayansalinas2345@gmail.com>
2024-06-09 12:03:01 -0500

    Configuración inicial del proyecto

---
+++ backend/target/debug/deps/backend-0401b64dec70e76c.d
@@ -0,0 +1,5 @@
+C:\Users\Johs\Documents\Proyectos\StepCheck\backend\target\debug\deps\libbackend-0401b64dec70e76c.rmeta: src\main.rs
+
+C:\Users\Johs\Documents\Proyectos\StepCheck\backend\target\debug\deps\backend-0401b64dec70e76c.d: src\main.rs
+
+src\main.rs:
---
+++ backend/target/debug/deps/backend-75bcd811614b6012.d
@@ -0,0 +1,5 @@
+C:\Users\Johs\Documents\Proyectos\StepCheck\backend\target\debug\deps\libbackend-75bcd811614b6012.rmeta: src\main.rs
+
+C:\Users\Johs\Documents\Proyectos\StepCheck\backend\target\debug\deps\backend-75bcd811614b6012.d: src\main.rs
+
+src\main.rs:
